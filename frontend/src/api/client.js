a6010c2a7a5137ffcbe5fb13c7558e7ac8d8d54c
bhumikaa22 <bhumikawahane@gmail.com>
2026-07-08 12:13:05 +0530

    Hardcode production API URL

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://amazon-backend-td50.onrender.com/api",
+  baseURL: "https://amazon-backend-td50.onrender.com/api",
 });
 
 client.interceptors.request.use((config) => {
